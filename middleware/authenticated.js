cc5ddc93daa9b45c799e7f52792f90e1d030e4bd
FitrahVyto <145330366+FitrahVyto@users.noreply.github.com>
2024-03-18 14:01:44 +0700

    fix

---
+++ middleware/authenticated.js
@@ -0,0 +1,27 @@
+const CryptoJS = require("crypto-js");
+
+
+export default function ({ redirect, store, app }) {
+    if (app.$cookies.get('credentials')) {
+        try {
+            const bytes  = CryptoJS.AES.decrypt(app.$cookies.get('credentials'), process.env.SALT_KEY);
+            const originalText = bytes.toString(CryptoJS.enc.Utf8);
+            if (originalText && location.pathname.startsWith("/login")) {
+                location.href = '/'
+            }
+            else if(!originalText && !location.pathname.startsWith("/login")) {
+                location.href = '/login'
+            }
+            store.dispatch('auth/actionSetCredentials', originalText)
+        } catch (error) {
+            app.$cookies.remove('credentials')
+            location.href = '/login'
+            // return redirect('/')
+        }
+    }    
+    else {
+        if(!location.pathname.startsWith("/login")) {
+            location.href = '/login'
+        }
+    }
+}
